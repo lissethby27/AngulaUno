3d776af2833bb94e936a4bb010b38ccf806c81d5
Lisseth <lbenturayuca@safareyes.es>
2024-11-15 17:55:12 +0100

    subida con los ultimos cambios ejercicio 2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {provideAnimations} from '@angular/platform-browser/animations';
+import {provideCharts, withDefaultRegisterables} from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
-    provideAnimations(), provideAnimationsAsync()]
+    provideAnimations(), provideAnimationsAsync(), provideCharts(withDefaultRegisterables())]
 };

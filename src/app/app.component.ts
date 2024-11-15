5a87479777658f6b487346571aa76d5e6a8b8c66
Lisseth <lbenturayuca@safareyes.es>
2024-11-15 17:38:20 +0100

    subida con los ultimos cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,48 @@ import {TarjetaComponent} from '../tarjeta/tarjeta.component';
 import {CalculadoraComponent} from '../calculadora/calculadora.component';
 import {CompraComponent} from '../compra/compra.component';
 import {FechaComponent} from '../fecha/fecha.component';
+import {AleatorioComponent} from '../aleatorio/aleatorio.component';
+import {NavegacionComponent} from '../navegacion/navegacion.component';
+import {Ventana1Component} from '../ventana1/ventana1.component';
+import {Ventana2Component} from '../ventana2/ventana2.component';
+import {CookiesComponent} from '../cookies/cookies.component';
+import { NotasComponent } from '../notas/notas.component';
+import {GaleriaComponent} from '../galeria/galeria.component';
+import {TareasComponent} from '../tareas/tareas.component';
+import {CalculadoraComprasComponent} from '../calculadora-compras/calculadora-compras.component';
+import {EncuestaComponent} from '../encuesta/encuesta.component';
+import {ContactosComponent} from '../contactos/contactos.component';
+import {DashboardComponent} from '../dashboard/dashboard.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, PruebaComponent, SaludoComponent,ContadorComponent,
-    ListaTareasComponent, TemporizadorComponent, ComentariosComponent, TarjetaComponent,
-    CalculadoraComponent, CompraComponent, FechaComponent],
+  imports: [RouterOutlet,
+    CommonModule,
+    PruebaComponent,
+    SaludoComponent,
+    ContadorComponent,
+    ListaTareasComponent,
+    TemporizadorComponent,
+    ComentariosComponent,
+    TarjetaComponent,
+    CalculadoraComponent,
+    CompraComponent,
+    FechaComponent,
+    AleatorioComponent,
+    NavegacionComponent,
+    Ventana1Component,
+    Ventana2Component,
+    CookiesComponent,
+    NotasComponent,
+    GaleriaComponent,
+    TareasComponent,
+    CalculadoraComprasComponent,
+    EncuestaComponent,
+    ContactosComponent,
+    DashboardComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
